2d070895fbaeaeacdc601d0ff64f9ae80cace326
poojithp <poojithp@host.docker.internal>
2020-06-25 12:17:57 +0530

    second time commit

--- springBootSwagger/src/main/java/com/studentDetails/springBootSwagger/swagger/SwaggerConfig.java
+++ springBootSwagger/src/main/java/com/studentDetails/springBootSwagger/swagger/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.student.demo.controller")).paths(regex("/api/v/1/.*"))
+				.apis(RequestHandlerSelectors.basePackage("com.studentDetails.springBootSwagger")).paths(regex("/api/v/1/.*"))
 				.build().apiInfo(metaData());
 	}
 
